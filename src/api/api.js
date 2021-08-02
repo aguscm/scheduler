b3ba451e98e161eddd1e35bd6cc67d999e0f5f6b
Agus <aguscm23@gmail.com>
2021-08-02 02:07:34 +0200

    Advancing in sending new events to API

--- src/api/api.js
+++ src/api/api.js
@@ -4,11 +4,29 @@ import axios from "axios";
 var url = "http://localhost:5000"
 
 //DATA
-//Gets the events
-async function getEvents() {
-    const res = await axios.get(url + '/');
+//Gets the calendars
+async function getCalendars() {
+    const res = await axios.get(url + '/calendars');
     return await res.data;
 }
 
+//Gets the events
+async function getEvents() {
+    const res = await axios.get(url + '/events');
+    return await res.data;
+}
+
+//Edits an event data that matches with the ID passed as a parameter
+async function editEvent(eventId, event) {
+    const res = await axios.put(url + `/events/${eventId}`, event);
+    return res;
+}
+
+//Adds a new foundation
+async function newEvent(event) {
+    console.log(event);
+    const res = await axios.post(url + '/events/', event)
+    return res;
+}
 //EXPORTS
-export default { getEvents };
+export default { getCalendars, getEvents, editEvent, newEvent };
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,13 @@
 <template>
   <p @click="showModal">Selected event {{ selectedEvent }}</p>
-  {{ events }}
-  {{ selectedEvent }}
+  <button
+    type="button"
+    class="btn btn-primary"
+    data-bs-toggle="modal"
+    data-bs-target="#eventDetailsModal"
+  >
+    New event
+  </button>
   <div class="container">
     <vue-cal
       :time-from="7 * 60"
@@ -18,10 +24,10 @@
         delete: false,
         create: true,
       }"
-      :events="!loading? events : undefined"
+      :events="!loading ? events : undefined"
       @event-drop="onEventDragCreate"
       @event-click="onEventClick"
-      @ready="loadEvents" 
+      @ready="loadEvents"
     />
   </div>
 
@@ -47,15 +53,31 @@
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="exampleModalLabel">Event details</h5>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
+          <p>Created on {{formatDate(selectedEvent.creationDate)}}</p>
         </div>
         <div class="modal-body">
           <form action="">
+            <div class="mb-3 row">
+              <label for="calendar" class="col-sm-2 col-form-label"
+                >Calendar<span class="text-danger">*</span></label
+              >
+              <div class="col-sm-10">
+                <select
+                  aria-label="calendar"
+                  name="calendar"
+                  class="form-select mb-3"
+                  v-model="selectedEvent.calendar"
+                >
+                  <option
+                    v-for="calendar in calendars"
+                    :key="calendar.id"
+                    :value="calendar.id"
+                  >
+                    {{ calendar.name }}
+                  </option>
+                </select>
+              </div>
+            </div>
             <div class="mb-3 row">
               <label for="name" class="col-sm-2 col-form-label"
                 >Name <span class="text-danger">*</span></label
@@ -71,14 +93,43 @@
             </div>
             <div class="mb-3 row">
               <label for="name" class="col-sm-2 col-form-label"
-                >Start date <span class="text-danger">*</span></label
+                >Date<span class="text-danger">*</span></label
               >
               <div class="col-sm-10">
                 <input
                   class="form-control"
-                  type="text"
-                  v-model="selectedEvent.start"
+                  type="date"
+                  v-model="startDateForm"
+                  @change="formatDateInForm()"
                   placeholder="Write here Foundation's name"
+                />
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="name" class="col-sm-2 col-form-label"
+                >Start time<span class="text-danger">*</span></label
+              >
+              <div class="col-sm-10">
+                <input
+                  class="form-control"
+                  type="time"
+                  v-model="startTimeForm"
+                  @change="formatDateInForm()"
+                  placeholder="Start time"
+                />
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="name" class="col-sm-2 col-form-label"
+                >End time<span class="text-danger">*</span></label
+              >
+              <div class="col-sm-10">
+                <input
+                  class="form-control"
+                  type="time"
+                  v-model="endTimeForm"
+                  @change="formatDateInForm()"
+                  placeholder="Start time"
                 />
               </div>
             </div>
@@ -96,9 +147,9 @@
             type="button"
             class="btn btn-primary"
             data-bs-dismiss="modal"
-            @click="updateEvent(selectedEvent)"
+            @click="newEvent(selectedEvent)"
           >
-            Save changes
+            Create event
           </button>
         </div>
       </div>
@@ -122,18 +173,36 @@ export default {
     msg: String,
   },
   created() {
-
+    this.loadCalendars();
   },
   data() {
     return {
-      selectedEvent: "",
+      selectedEvent: {
+        calendar: "",
+        title: "",
+        start: "",
+        end: "",
+        status: "",
+        class: "",
+        content: "",
+        contentFull: "",
+        icon: "",
+        creationDate: "",
+        applicant: "",
+      },
+
+      //Form
+      startDateForm: "",
+      startTimeForm: "",
+      endTimeForm: "",
       //Loading and error variables
       loading: true,
       error: false,
       errorMsg: "",
       /**/
       showDialog: true,
-      events: ""
+      events: "",
+      calendars: "",
     };
   },
   methods: {
@@ -144,9 +213,7 @@ export default {
       return (
         API.getEvents()
           .then(
-            (response) => (
-              (this.events = response), (this.loading = false)
-            )
+            (response) => ((this.events = response), (this.loading = false))
           )
           //If error
           .catch(
@@ -156,8 +223,70 @@ export default {
           )
       );
     },
+    loadCalendars() {
+      //Loads the main foundations database
+      this.foundations = "";
+      this.loading = true;
+      return (
+        API.getCalendars()
+          .then((response) => (this.calendars = response))
+          //If error
+          .catch(
+            (err) => (
+              console.log(err), (this.isError = true), (this.errorMsg = err)
+            )
+          )
+      );
+    },
+    loadFormSelectedEvent(id) {
+      this.clearSelectedEvent();
+      for (var index in this.events) {
+        if (this.events[index].id == id) {
+          this.selectedEvent.calendar = this.events[index].calendar;
+          this.selectedEvent.title = this.events[index].title;
+          this.selectedEvent.start = this.events[index].start;
+          this.selectedEvent.end = this.events[index].end;
+          this.selectedEvent.status = this.events[index].status;
+          this.selectedEvent.content = this.events[index].content;
+          this.selectedEvent.contentFull = this.events[index].contentFull;
+          this.selectedEvent.icon = this.events[index].icon;
+          this.selectedEvent.creationDate = this.events[index].creationDate;
+          this.selectedEvent.applicant = this.events[index].applicant;
+
+          this.startDateForm = this.selectedEvent.start.split(" ")[0];
+          this.startTimeForm = this.selectedEvent.start.split(" ")[1];
+          this.endTimeForm = this.selectedEvent.end.split(" ")[1];
+          break;
+        }
+      }
+    },
+    formatDate(time) {
+      var date_ob = new Date(time);
+      let date = date_ob.getDate();
+      let month = date_ob.getMonth() + 1;
+      let year = date_ob.getFullYear();
+
+      let hour = date_ob.getHours();
+      let minutes = date_ob.getMinutes();
+      if (minutes < 10) {
+        minutes = "0" + minutes;
+      }
+
+      return date + "/" + month + "/" + year + "-" + hour + ":" + minutes;
+    },
+    //Send a request to the server to create a new foundation
+    newEvent(event) {
+      return API.newEvent(event)
+        .then(() => this.loadEvents())
+        .catch((err) => console.log(err));
+    },
+    formatDateInForm() {
+      this.selectedEvent.start = this.startDateForm + " " + this.startTimeForm;
+      this.selectedEvent.end = this.startDateForm + " " + this.endTimeForm;
+    },
     async onEventClick(event) {
-      this.selectedEvent = this.events[this.findEventInEvents(event)];
+      console.log(event.id);
+      this.loadFormSelectedEvent(event.id);
       this.showModal();
 
       // Prevent navigating to narrower view (default vue-cal behavior).
@@ -172,24 +301,23 @@ export default {
       // return event;
       console.log(event.event);
     },
-    findEventInEvents(event) {
-      console.log(event.id);
-      var index = this.events.findIndex(
-        (foundEvent) => foundEvent.id == event.event.id
-      );
-      return index;
-    },
-    updateEvent(event) {
-      var index = this.events.findIndex(
-        (foundEvent) => foundEvent.id == event.id
-      );
-      this.events[index] = event.event;
-    },
     async showModal() {
       var eventDetailsModal = new Modal(
         document.getElementById("eventDetailsModal")
       );
       eventDetailsModal.toggle();
+    },
+    clearSelectedEvent() {
+      this.selectedEvent.calendar = "";
+      this.selectedEvent.title = "";
+      this.selectedEvent.start = "";
+      this.selectedEvent.end = "";
+      this.selectedEvent.status = "";
+      this.selectedEvent.content = "";
+      this.selectedEvent.contentFull = "";
+      this.selectedEvent.icon = "";
+      this.selectedEvent.creationDate = "";
+      this.selectedEvent.applicant = "";
     },
   },
 };
